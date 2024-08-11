c706e614f5aba22ab865c46d7af6869ad4c021b2
kingno3l <nuelluiz@gmail.com>
2024-08-11 05:51:21 +0100

    added the get and post endpoints to my api file in routes folder

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LabTestController;
+use App\Http\Controllers\MedicalDataController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:api')->get('/lab-tests', [LabTestController::class, 'index']);
+Route::middleware('auth:api')->post('/submit-medical-data', [MedicalDataController::class, 'store']);
+
